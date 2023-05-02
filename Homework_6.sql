4c7dac86ecf27f2a01cd55faed65a14f5f7a528b
DenisPotorsky <96338996+DenisPotorsky@users.noreply.github.com>
2023-05-02 10:41:20 +0500

    Add files via upload

---
+++ Homework_6.sql
@@ -0,0 +1,53 @@
+-- 1.	Создайте функцию, которая принимает кол-во сек 
+--      и форматирует их в кол-во дней, часов, минут и секунд.
+
+--      Пример: 123456 ->'1 days 10 hours 17 minutes 36 seconds '
+
+use homework_6;
+ 
+DELIMITER $$
+CREATE PROCEDURE times(seconds INT)
+BEGIN
+    DECLARE days INT default 0;
+    DECLARE hours INT default 0;
+    DECLARE minutes INT default 0;
+
+    WHILE seconds >= 86400 DO
+    SET days = seconds / 86400;
+    SET seconds = seconds % 86400;
+    END WHILE;
+
+    WHILE seconds >= 3600 DO
+    SET hours = seconds / 3600;
+    SET seconds = seconds % 3600;
+    END WHILE;
+
+    WHILE seconds >= 60 DO
+    SET minutes = seconds / 60;
+    SET seconds = seconds % 60;
+    END WHILE;
+
+SELECT days, hours, minutes, seconds;
+END $$
+DELIMITER ;
+
+CALL times(123456);
+
+
+DELIMITER $$
+CREATE PROCEDURE numbers()
+BEGIN
+    DECLARE n INT default 0;
+    DROP TABLE IF EXISTS nums;
+    CREATE TABLE nums (n INT);
+
+    WHILE n < 10 DO
+    SET n = n + 2;
+    INSERT INTO nums VALUES(n);
+    END WHILE;
+
+SELECT * FROM nums;
+END $$
+DELIMITER ;
+
+CALL numbers();
